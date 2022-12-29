db0d33d7eef96c0935e27738fce8324e6abb42cc
AEmin321 <ahmademin321@gmail.com>
2022-12-29 18:39:24 +0300

    solved 1-6 problems

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    const inputArray=args[0];
+    return inputArray.filter(function(val){
+        return !args.includes(val);
+    })
 };
 
 // Do not edit below this line
